093b139abafcf15e5a9b05df1026531fe717b717
B-Shankar <learn.bhimashankar@gmail.com>
2025-11-07 00:11:14 +0530

    setup

---
+++ src/main/java/in/bhimashankar/airdrive/AirdriveApplication.java
@@ -0,0 +1,13 @@
+package in.bhimashankar.airdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirdriveApplication.class, args);
+	}
+
+}
