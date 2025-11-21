ba2a9bc3ebc0d509fb1e84ad7999b1553411d0cf
B-Shankar <learn.bhimashankar@gmail.com>
2025-11-22 00:04:45 +0530

    implemented payment-history api's for transaction section

--- src/main/java/in/bhimashankar/airdrive/repository/PaymentTransactionRepository.java
+++ src/main/java/in/bhimashankar/airdrive/repository/PaymentTransactionRepository.java
@@ -4,11 +4,16 @@ import in.bhimashankar.airdrive.document.PaymentTransaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PaymentTransactionRepository extends MongoRepository<PaymentTransaction, String> {
     List<PaymentTransaction> findByClerkId(String clerkId);
 
     List<PaymentTransaction> findByClerkIdOrderByTransactionDateDesc(String clerkId);
 
-    List<PaymentTransaction> findByClerkIdAndStatusOrderByTransactionDateDesc(String clerkId);
+    List<PaymentTransaction> findByClerkIdAndStatusOrderByTransactionDateDesc(String clerkId, String status);
+
+    Optional<PaymentTransaction> findByOrderId(String orderId);
+
+    Optional<PaymentTransaction> findByPaymentId(String paymentId);
 }
