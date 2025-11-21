b9ee071ffcd18e6a0e52704fe99197b2df09f246
B-Shankar <learn.bhimashankar@gmail.com>
2025-11-22 00:05:27 +0530

    method correction

--- src/main/java/in/bhimashankar/airdrive/controller/FileController.java
+++ src/main/java/in/bhimashankar/airdrive/controller/FileController.java
@@ -4,22 +4,30 @@ import in.bhimashankar.airdrive.dto.FileMetadataDTO;
 import in.bhimashankar.airdrive.service.FileMetadataService;
 import in.bhimashankar.airdrive.service.UserCreditsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/files")
@@ -68,8 +76,9 @@ public class FileController {
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/{id}/toggle-public")
+    @PostMapping("/{id}/toggle-public")
     public ResponseEntity<?> togglePublic(@PathVariable String id) {
+        log.info("In Toggle Public");
         FileMetadataDTO file = fileMetadataService.togglePublic(id);
         return ResponseEntity.ok(file);
     }
